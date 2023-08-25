d42babef9218a1f65de6a7f597f48bc9be9823a5
Annu <ananya20704@gmail.com>
2023-08-25 18:39:43 +0530

    Distance between two points

---
+++ DistanceBetween2Points.py
@@ -0,0 +1,6 @@
+x1=int(input("x coordinate of 1st point:"))
+y1=int(input("y coordinate of 1st point:"))
+x2=int(input("x coordinate of 2nd point:"))
+y2=int(input("y coordinate of 2nd point:"))
+distance=((x2-x1)**2+(y2-y1)**2)**0.5
+print("Distnce between two points is",distance)
