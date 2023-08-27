97f6bfe86b39471e0be99e92fcac567093ab6475
Annu <ananya20704@gmail.com>
2023-08-27 16:42:07 +0530

    Table of any number

--- practise.py
+++ practise.py
@@ -1,6 +1,8 @@
-rad =3
-pi=3.14
-circumference =2*pi*rad
-print(circumference)
-area = pi*rad*rad
-print(area)
+Eng = int(input("Enter marks of English:"))
+GK = int(input("Enter marks of GK:"))
+EVS = int(input("Enter marks of EVS:"))
+Per = (Eng+GK+EVS)/3
+if(Per>=50):
+    print("pass")
+else:
+    print("fail")
