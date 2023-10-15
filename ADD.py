1ce5915c7a201dca6c7c323543a27db43a817d47
Annu <ananya20704@gmail.com>
2023-10-15 12:47:15 +0530

    IF_ELSE program

--- ADD.py
+++
@@ -1,6 +0,0 @@
-#addition of two number by taking input from user
-
-A = int(input("Enter first number:"))
-B = int(input("Enter second number:"))
-c = A+B
-print(c)
--- if-else.py
+++ if-else.py
@@ -1,7 +1,23 @@
-a = 25
-b = 36
+# print greatest number
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
 if(a>b):
     print("a is greater than b")
 else:
     print("a is smaller then b")
     
+#to print consonant or vowel
+V= input("Enter alphabet:")
+if(V=='A') or (V=="E") or (V=='I') or (V=='O') or (V=='U') or (V=="a") or (V=="e") or (V=="i") or (V=="o") or (V=='u'):
+    print("vowel")
+else:
+    print("consonant")
+
+
+#print number is even or odd
+
+a = int(input("Enter your number:"))
+if(a%2==0):
+    print("a is an even number")
+else:
+    print("a is an odd number")
